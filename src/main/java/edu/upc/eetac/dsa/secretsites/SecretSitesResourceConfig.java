4912a97b75422024de14d8d318c5c2d1eb87b7da
Marti <marti>
2016-03-24 20:42:48 +0100

    Primeros retoques y dependencias

--- src/main/java/edu/upc/eetac/dsa/secretsites/SecretSitesResourceConfig.java
+++ src/main/java/edu/upc/eetac/dsa/secretsites/SecretSitesResourceConfig.java
@@ -1,7 +1,13 @@
 package edu.upc.eetac.dsa.secretsites;
 
+import org.glassfish.jersey.server.ResourceConfig;
+
 /**
  * Created by Marti on 24/03/2016.
  */
-public class SecretSitesResourceConfig {
+public class SecretSitesResourceConfig extends ResourceConfig {
+
+    public SecretSitesResourceConfig() {
+        packages("edu.upc.eetac.dsa.secretsites");
+    }
 }
